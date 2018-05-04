8d7ab1193aaf05ab963f57cdb0af789bc91392bb
pedroazevedo <prgazevedo@gmail.com>
2018-05-04 10:05:49 +0100

    Add HashCode and Equals to ensure HashMap functioning

--- src/main/java/com/company/graph/Coordinates2D.java
+++ src/main/java/com/company/graph/Coordinates2D.java
@@ -1,5 +1,7 @@
 package com.company.graph;
 
+
+
 public class Coordinates2D {
 
 
@@ -35,4 +37,36 @@ public class Coordinates2D {
             this.x  = x;
             this.y = y;
         }
+
+
+    private int m_hash = 0;
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     */
+   public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj instanceof Coordinates2D) {
+            Coordinates2D other = (Coordinates2D) obj;
+            return getX() == other.getX() && getY() == other.getY();
+        } else return false;
+    }
+    /**
+     * Returns a hash code value for the point.
+     */
+    public int hashCode() {
+        if (m_hash == 0) {
+            long bits = 7L;
+            bits = 31L * bits + Double.doubleToLongBits(getX());
+            bits = 31L * bits + Double.doubleToLongBits(getY());
+            m_hash = (int) (bits ^ (bits >> 32));
+        }
+        return m_hash;
+    }
+
+    /**
+     * Returns a string representation
+     */
+     public String toString() {
+        return "Coordinates2D [x = " + getX() + ", y = " + getY() + "]";
+    }
 }
